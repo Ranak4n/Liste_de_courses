52bf1ea391b243eb0d78db760c92b31425a9a417
Antoine Loigerot <antoine.loigerot@stfelixlasalle.fr>
2023-12-17 21:58:13 +0100

    add zone page

--- src/Controller/ZoneController.php
+++ src/Controller/ZoneController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Zone;
+use App\Repository\ZoneRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ZoneController extends AbstractController
 {
-    #[Route('/zone', name: 'app_zone')]
+    
+    private ZoneRepository $zoneRepository;
+
+    public function __construct(ZoneRepository $zoneRepository)
+    {
+        $this->zoneRepository = $zoneRepository;
+    }
+
+    #[Route('/zones', name: 'zone_index')]
     public function index(): Response
     {
+        $zones = $this->zoneRepository->findAll();
+
         return $this->render('zone/index.html.twig', [
-            'controller_name' => 'ZoneController',
+            'zones' => $zones,
         ]);
     }
 }
