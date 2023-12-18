c9044268fe23d5ec643bffb4689f31da1fa55903
Antoine Loigerot <antoine.loigerot@stfelixlasalle.fr>
2023-12-18 15:42:33 +0100

    Add form + style

--- src/Controller/ZoneController.php
+++ src/Controller/ZoneController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Zone;
 use App\Repository\ZoneRepository;
+use App\Form\ZoneType;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ZoneController extends AbstractController
 {
@@ -26,6 +28,25 @@ class ZoneController extends AbstractController
 
         return $this->render('zone/index.html.twig', [
             'zones' => $zones,
+        ]);
+    }
+
+    #[Route('/new', name: 'zone_new', methods: ["GET", "POST"])]
+    public function new(Request $request): Response {
+
+        $zone = new Zone();
+
+        $form = $this->createForm(ZoneType::class, $zone);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->zoneRepository->save($zone, true);
+            return $this->redirectToRoute('zone_index', [], Response::HTTP_SEE_OTHER);
+        }
+        
+        return $this->render('zone/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Repository/ZoneRepository.php
+++ src/Repository/ZoneRepository.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace App\Repository;
-
 use App\Entity\Zone;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-
 /**
  * @extends ServiceEntityRepository<Zone>
  *
@@ -20,29 +17,18 @@ class ZoneRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Zone::class);
     }
-
-//    /**
-//     * @return Zone[] Returns an array of Zone objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('z')
-//            ->andWhere('z.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('z.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Zone
-//    {
-//        return $this->createQueryBuilder('z')
-//            ->andWhere('z.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function save(Zone $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+    public function remove(Zone $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
 }
