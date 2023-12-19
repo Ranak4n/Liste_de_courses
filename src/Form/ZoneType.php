18fca2f7b7f16b00f0ff1b1eb6c15c289a9795d9
Antoine Loigerot <antoine.loigerot@stfelixlasalle.fr>
2023-12-19 09:41:43 +0100

    Add CRUD conf w/o style

--- src/Form/ZoneType.php
+++ src/Form/ZoneType.php
@@ -3,7 +3,6 @@
 namespace App\Form;
 
 use App\Entity\Zone;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
